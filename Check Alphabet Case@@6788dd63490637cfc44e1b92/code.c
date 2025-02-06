9c51a52e720b54afe2adeb6fa9ed96c77662998e
GarimaGupta16 <garima0277.becse24@chitkara.edu.in>
2025-02-06 12:40:00 +0530

    Solution to Check Alphabet Case

---
+++ Check Alphabet Case@@6788dd63490637cfc44e1b92/code.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+
+    char n;
+    scanf("%d",&n);
+    if(char>='A'  && char<='Z' ){
+        printf("Uppercase");
+    }
+   else{
+        printf("Lowecase");
+    }
+
+    return 0;
+}
