b5cd3ed8eb2ede36c1aed95e74980ee35f53cad9
GarimaGupta16 <garima0277.becse24@chitkara.edu.in>
2025-03-30 19:40:11 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,9 +1,11 @@
 int fibonacciSeries(int n ){
+    int result=0;
     if(n==0){
         return 0;
     }
     if(n==1){
         return 1;
     }
-    return fibonacciSeries(n-1) + fibonacciSeries(n-2);
+    return result = fibonacciSeries(n-1) + fibonacciSeries(n-2);
+    printf("%d ", result);
 }
